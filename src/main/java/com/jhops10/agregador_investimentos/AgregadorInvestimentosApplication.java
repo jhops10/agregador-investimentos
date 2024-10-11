d21dfd43c4d8b1c115e4d2d72785f87f5916862d
João Paulo <joaowais@gmail.com>
2024-10-11 11:55:37 -0300

    first commit

---
+++ src/main/java/com/jhops10/agregador_investimentos/AgregadorInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.agregador_investimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorInvestimentosApplication.class, args);
+	}
+
+}
