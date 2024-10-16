7f4bb071e0ef525f0b9c4b9c7c6bf86037f90c19
João Paulo <joaowais@gmail.com>
2024-10-16 12:19:48 -0300

    add Stock entity

---
+++ src/main/java/com/jhops10/agregador_investimentos/entity/Stock.java
@@ -0,0 +1,42 @@
+package com.jhops10.agregador_investimentos.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_stocks")
+public class Stock {
+
+    @Id
+    private String stockId;
+
+    private String description;
+
+
+    public Stock() {
+    }
+
+    public Stock(String stockId, String description) {
+        this.stockId = stockId;
+        this.description = description;
+    }
+
+    public String getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(String stockId) {
+        this.stockId = stockId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
